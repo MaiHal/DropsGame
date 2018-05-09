424dfb08fe966caba9fa6366197561f361437f39
MaiHal <myturn.lovenyc@gmail.com>
2018-05-10 00:36:45 +0900

    スコアバーの長さをスクリプトから変更する処理

---
+++ Assets/Script/ScoreBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBar : MonoBehaviour {
+	LineRenderer scoreBar;
+	// Use this for initialization
+	void Start () {
+		scoreBar = GameObject.Find("Score").GetComponent<LineRenderer>();
+		float x = -10.0f;
+		Vector3 point = new Vector3(x, 3.2f, 0);
+		//-6 ~ -11.3
+		scoreBar.SetPosition(1, point);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
