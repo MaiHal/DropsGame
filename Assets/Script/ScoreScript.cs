a856c7b7b1472be75a1d2559c5a679abcb290c7a
MaiHal <myturn.lovenyc@gmail.com>
2018-05-09 14:05:30 +0900

    スクリプトの数値をUIの数値に反映させる処理を作成

---
+++ Assets/Script/ScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+	public Text scoreText;
+	public int score;
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		countScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = score.ToString();
+	}
+
+	void countScore(){
+		score = 10;
+	}
+}
