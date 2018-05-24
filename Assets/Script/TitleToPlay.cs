853958f5a49bc9fb6f55f2cbfe6d2df51a98836b
MaiHal <myturn.lovenyc@gmail.com>
2018-05-24 15:47:36 +0900

    ボタンの配置

---
+++ Assets/Script/TitleToPlay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleToPlay : MonoBehaviour {
+
+	public void moveToPlay(){
+		SceneManager.LoadScene ("Main");
+	}
+}
